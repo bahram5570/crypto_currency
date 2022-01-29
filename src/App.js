3504178629cbc100a8d974bb6e7c707d2f3d6254
bahram5570 <bahramsa70@gmail.com>
2022-01-29 12:08:52 +0330

    qqq

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function App() {
+  const [a, aaa] = useState("");
+
+  useEffect(async () => {
+    try {
+      const response = await axios({
+        method: "get",
+        url: "https://coinranking1.p.rapidapi.com/coins",
+        params: {
+          referenceCurrencyUuid: "yhjMzLPhuIDl",
+          timePeriod: "24h",
+          tiers: "1",
+          orderBy: "marketCap",
+          orderDirection: "desc",
+          limit: "50",
+          offset: "0",
+        },
+        headers: {
+          "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+          "x-rapidapi-key":
+            "e5d3f6fec5mshf354b8b7225d159p1cbd23jsn2a64234d5c2f",
+        },
+      });
+      aaa(response.data.data.coins[0].price);
+      
+    } catch (error) {
+      aaa("Error.................");
+    }
+  }, []);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      axios
+      <h1>{a}</h1>
     </div>
   );
 }
